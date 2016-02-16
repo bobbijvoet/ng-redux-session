6a62ccf4591ae840fa8f141a7f8ac183f0c405a0
Achraf Belmokadem <a.belmokadem@gmail.com>
2016-02-16 12:53:05 +0100

    Remove unnecessary text import

--- app/app.ts
+++ app/app.ts
@@ -3,8 +3,6 @@ import 'angular-resource';
 
 import 'app.module.ts';
 
-import template from './app.html!text';
-
 import 'components/main/main.module.ts';
 import 'components/story/story.module.ts';
 import 'components/header/header.module.ts';
