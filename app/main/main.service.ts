21df20e92579f4a998239e8c02557b352d44f2a4
Bob Bijvoet <bob@frontmen.nl>
2016-02-16 13:08:33 +0100

    Added mocking

--- app/main/main.service.ts
+++ app/main/main.service.ts
@@ -6,35 +6,17 @@ function demoService($http, $q, mainConstant) {
     };
 
     function getStories() {
-        return $q.when({
-            stories: [
-                {
-                    points: 1,
-                    description: 'Some lil story',
-                    done: false
-                },
-                {
-                    points: 8,
-                    description: 'Some bigger story',
-                    done: false
-                },
-                {
-                    points: 20,
-                    description: 'Some huge story',
-                    done: false
-                }
-            ]
-        });
-        //return $http.get(`${mainConstant.endpoint}/phones`, {})
-        //    .then(getStoriesSuccess, getStoriesFail);
-        //
-        //function getStoriesSuccess(response) {
-        //    return {};
-        //}
-        //
-        //function getStoriesFail(error) {
-        //    return $q.reject(error);
-        //}
+        return $http.get(`${mainConstant.endpoint}/sprint`, {})
+            .then(getStoriesSuccess, getStoriesFail);
+
+        function getStoriesSuccess(response) {
+            return response.data;
+        }
+
+        function getStoriesFail(error) {
+            console.log(error);
+            return $q.reject(error);
+        }
     }
 }
 
