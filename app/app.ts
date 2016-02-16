b13bab47ab05b08c19ace0e50ec1d63533383d72
Bob Bijvoet <bob@frontmen.nl>
2016-02-16 16:48:04 +0100

    Added redux example

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,8 @@
 import 'angular';
 import 'angular-resource';
 import 'ng-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
 
 import {HeaderComponent} from "./components/header/header.component.ts";
 import {MainComponent} from "./components/main/main.component.ts";
@@ -12,6 +14,27 @@ import {NotificationComponent} from "./components/notification/notification.comp
 import {SprintComponent} from "./components/sprint/sprint.component.ts";
 
 var app = angular.module('app', ['ngRedux']);
+
+function counter(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state
+    }
+}
+
+let store = createStore(counter);
+
+store.subscribe(() =>
+    console.log(store.getState())
+);
+
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'DECREMENT'});
 
 // Components
 app.component('headerComponent', HeaderComponent);
