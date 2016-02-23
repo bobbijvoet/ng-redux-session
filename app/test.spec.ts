41059fa1f58698e436dbcc1777e89de0ac64e917
Bob Bijvoet <bob@frontmen.nl>
2016-02-23 12:21:05 +0100

    Finetuned karma config

---
+++ app/test.spec.ts
@@ -0,0 +1,12 @@
+describe('given the sprint component', function () {
+
+
+    describe('when initializing with valid token', function () {
+
+        it('should set the view to have a valid token', function () {
+
+            expect(true).toBe(true);
+        });
+    });
+
+});
