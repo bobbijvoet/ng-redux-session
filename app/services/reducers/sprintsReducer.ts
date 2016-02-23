13c5e208d4dbe1bffb3de5465c2369fbc0f10f9c
Achraf Belmokadem <a.belmokadem@gmail.com>
2016-02-23 10:47:52 +0100

    Update reducers

--- app/services/reducers/sprintsReducer.ts
+++ app/services/reducers/sprintsReducer.ts
@@ -1,5 +1,19 @@
-function sprintsReducer(state, action) {
+var initialState = {
+    sprints: []
+};
 
+function sprintsReducer(state, action) {
+    if (typeof state === 'undefined') {
+        return initialState
+    }
+
+    switch (action.type) {
+        case 'FETCH_SPRINT_FULFILLED': {
+            state.sprints = [action.payload];
+        }
+    }
+
+    return state;
 }
 
 export {sprintsReducer};
