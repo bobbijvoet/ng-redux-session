8c09389f872db42ac9a25f61040fc6a6aef736c3
Bob Bijvoet <bob@frontmen.nl>
2016-02-23 13:17:36 +0100

    Initial tests

---
+++ app/services/reducers/sprintsReducer.spec.ts
@@ -0,0 +1,22 @@
+import {sprintsReducer} from "./sprintsReducer.ts";
+
+
+describe('given the sprint reducer', function () {
+
+    describe('when initializing', function () {
+        var state = sprintsReducer(undefined,undefined);
+
+        it('should return the initial state', function () {
+            expect(state).toEqual({sprints: []});
+        });
+    });
+
+    describe('when fetched a sprint', function () {
+        var state = sprintsReducer({}, {type: 'FETCH_SPRINT_FULFILLED', payload: ['a', 'b', 'c']});
+
+        it('should populate the sprint array', function () {
+            expect(state).toEqual({sprints: [['a', 'b', 'c']]});
+        });
+    });
+
+});
