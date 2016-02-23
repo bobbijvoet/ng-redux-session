18b538286812007f70604a285b4692e0e931b54b
Achraf Belmokadem <a.belmokadem@gmail.com>
2016-02-23 16:22:32 +0100

    Small refactoring

--- app/components/notification/notification.component.ts
+++ app/components/notification/notification.component.ts
@@ -4,26 +4,7 @@ var NotificationComponent = {
     bindings: {
         message: '='
     },
-    controller: NotificationController,
     template: template
 };
 
 export {NotificationComponent};
-
-NotificationController.$inject = ['$rootScope', '$timeout'];
-
-function NotificationController($rootScope, $timeout) {
-    var vm = this;
-
-    //$rootScope.$on('point', function (event, points) {
-    //    vm.message = `${points} points burned!`;
-    //    vm.visible = true;
-    //
-    //});
-    //
-    //$rootScope.$on('sprint', function (event, sprint) {
-    //    vm.message = `Sprint ${sprint} complete!`;
-    //});
-
-    //var hideNotification = $timeout
-}
